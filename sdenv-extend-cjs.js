61cafa4096b4db8a5bcf975f5e63843e8936604f
rnet <chinaitlearner@gmail.com>
2025-12-22 13:03:02 +0800

    自动更新 remote_build 内容

--- sdenv-extend-cjs.js
+++ sdenv-extend-cjs.js
@@ -2103,76 +2103,6 @@ var merge = createAssigner(function(object, source, srcIndex) {
   baseMerge(object, source, srcIndex);
 });
 
-const gv = {
-  adapt: {}, // 适配专用网站方法
-  tools: {}, // 内部工具
-  utils: {}, // 外部方法
-  config: {
-    // 强制setInterval的时间设置，如设置的足够大让执行函数不运行，其中：undefined表示通过框架拦截并计算运行时间，0表示不拦截且使用原时间
-    timeInterval: undefined,
-    timeTimeout: undefined, // 与timeInterval功能类似，区别是控制setTimeout
-    isNode: typeof window === 'undefined', // 标记是否是node环境初始化得到的sdenv
-    envType: 'browser', // 执行环境类型，node或者browser，初始化时赋予
-    // start window proxy
-    // 控制window返回undefined的属性名集合
-    windowGetterUndefinedKeys: ["_top","_parent","_length","_globalObject", "_sdGlobalObject","_globalProxy","_registeredHandlers","_eventHandlers","_resourceLoader","_document","_origin","_sessionHistory","_virtualConsole","_runScripts","_frameElement","_pretendToBeVisual","_storageQuota","_commonForOrigin","_currentOriginData","_localStorage","_sessionStorage","_selection","_customElementRegistry","loadTextSync"],
-    // 控制window返回not defined报错的的属性名集合
-    windowGetterErrorKeys: ['process'],
-    // 控制window返回自身的属性名集合
-    windowGetterWinKeys: ['window', 'top', 'self', 'frames', 'globalThis'],
-    // end window proxy
-  },
-  memory: {
-    runinfo: { // 程序运行时间节点
-      start: new Date().getTime(), // 代码开始运行
-      isDied: false, // 窗口生命是否已经结束
-    },
-    timeout: null, // 存放处理setTimeout/setInterval的类
-    SdenvExtend: null, // node环境缓存sdenv-extend插件，用于浏览器环境使用
-  },
-  cache: {
-    dynamicCode: [], // 缓存动态生成代码
-    runloop: {}, // 缓存循环代码运行时
-    /*
-      缓存循环执行顺序
-      sdenv.cache.runsArr
-        .map((it, num) => ({idx: it.idx, num, ...it.val}))
-        .reduce((ans, it) => (
-          ans[it.idx] === undefined ? ans[it.idx] = [it] : ans[it.idx].push(it), ans
-        ), {})
-     */
-    runsArr: [],
-    runsObj: [], // 缓存循环递归树
-    runtime: {}, // 存放运行时数据的代理
-    monitor: {}, // 监控变量对象
-    timeoutIdx: 0, // timeout编号，初始化时值递增
-    intervalIdx: 0, // interval编号，初始化时值递增
-  }, // 存储缓存数据
-  datas: {
-    dateAndRandom: {}, // 随机数相关数据
-  },
-};
-
-/**
- * A specialized version of `_.map` for arrays without support for iteratee
- * shorthands.
- *
- * @private
- * @param {Array} [array] The array to iterate over.
- * @param {Function} iteratee The function invoked per iteration.
- * @returns {Array} Returns the new mapped array.
- */
-function arrayMap(array, iteratee) {
-  var index = -1,
-      length = array == null ? 0 : array.length,
-      result = Array(length);
-
-  while (++index < length) {
-    result[index] = iteratee(array[index], index, array);
-  }
-  return result;
-}
-
 /** `Object#toString` result references. */
 var symbolTag$1 = '[object Symbol]';
 
@@ -2340,6 +2270,26 @@ var stringToPath = memoizeCapped(function(string) {
   return result;
 });
 
+/**
+ * A specialized version of `_.map` for arrays without support for iteratee
+ * shorthands.
+ *
+ * @private
+ * @param {Array} [array] The array to iterate over.
+ * @param {Function} iteratee The function invoked per iteration.
+ * @returns {Array} Returns the new mapped array.
+ */
+function arrayMap(array, iteratee) {
+  var index = -1,
+      length = array == null ? 0 : array.length,
+      result = Array(length);
+
+  while (++index < length) {
+    result[index] = iteratee(array[index], index, array);
+  }
+  return result;
+}
+
 /** Used as references for various `Number` constants. */
 var INFINITY$2 = 1 / 0;
 
@@ -2448,6 +2398,86 @@ function baseGet(object, path) {
   }
   return (index && index == length) ? object : undefined;
 }
+
+/**
+ * Gets the value at `path` of `object`. If the resolved value is
+ * `undefined`, the `defaultValue` is returned in its place.
+ *
+ * @static
+ * @memberOf _
+ * @since 3.7.0
+ * @category Object
+ * @param {Object} object The object to query.
+ * @param {Array|string} path The path of the property to get.
+ * @param {*} [defaultValue] The value returned for `undefined` resolved values.
+ * @returns {*} Returns the resolved value.
+ * @example
+ *
+ * var object = { 'a': [{ 'b': { 'c': 3 } }] };
+ *
+ * _.get(object, 'a[0].b.c');
+ * // => 3
+ *
+ * _.get(object, ['a', '0', 'b', 'c']);
+ * // => 3
+ *
+ * _.get(object, 'a.b.c', 'default');
+ * // => 'default'
+ */
+function get(object, path, defaultValue) {
+  var result = object == null ? undefined : baseGet(object, path);
+  return result === undefined ? defaultValue : result;
+}
+
+const gv = {
+  adapt: {}, // 适配专用网站方法
+  tools: {}, // 内部工具
+  utils: {}, // 外部方法
+  config: {
+    // 强制setInterval的时间设置，如设置的足够大让执行函数不运行，其中：undefined表示通过框架拦截并计算运行时间，0表示不拦截且使用原时间
+    timeInterval: undefined,
+    timeTimeout: undefined, // 与timeInterval功能类似，区别是控制setTimeout
+    isNode: typeof window === 'undefined', // 标记是否是node环境初始化得到的sdenv
+    envType: 'browser', // 执行环境类型，node或者browser，初始化时赋予
+    // start window proxy
+    // 控制window返回undefined的属性名集合
+    windowGetterUndefinedKeys: ["_top","_parent","_length","_globalObject", "_sdGlobalObject","_globalProxy","_registeredHandlers","_eventHandlers","_resourceLoader","_document","_origin","_sessionHistory","_virtualConsole","_runScripts","_frameElement","_pretendToBeVisual","_storageQuota","_commonForOrigin","_currentOriginData","_localStorage","_sessionStorage","_selection","_customElementRegistry","loadTextSync"],
+    // 控制window返回not defined报错的的属性名集合
+    windowGetterErrorKeys: ['process'],
+    // 控制window返回自身的属性名集合
+    windowGetterWinKeys: ['window', 'top', 'self', 'frames', 'globalThis'],
+    // end window proxy
+  },
+  memory: {
+    runinfo: { // 程序运行时间节点
+      start: new Date().getTime(), // 代码开始运行
+      isDied: false, // 窗口生命是否已经结束
+    },
+    timeout: null, // 存放处理setTimeout/setInterval的类
+    SdenvExtend: null, // node环境缓存sdenv-extend插件，用于浏览器环境使用
+  },
+  cache: {
+    dynamicCode: [], // 缓存动态生成代码
+    runloop: {}, // 缓存循环代码运行时
+    /*
+      缓存循环执行顺序
+      sdenv.cache.runsArr
+        .map((it, num) => ({idx: it.idx, num, ...it.val}))
+        .reduce((ans, it) => (
+          ans[it.idx] === undefined ? ans[it.idx] = [it] : ans[it.idx].push(it), ans
+        ), {})
+     */
+    runsArr: [],
+    runsObj: [], // 缓存循环递归树
+    runtime: {}, // 存放运行时数据的代理
+    monitor: {}, // 监控变量对象
+    timeoutIdx: 0, // timeout编号，初始化时值递增
+    intervalIdx: 0, // interval编号，初始化时值递增
+  }, // 存储缓存数据
+  datas: {
+    dateAndRandom: {}, // 随机数相关数据
+  },
+};
 
 /** Used to stand-in for `undefined` hash values. */
 var HASH_UNDEFINED = '__lodash_hash_undefined__';
@@ -3313,36 +3343,6 @@ function baseMatches(source) {
   return function(object) {
     return object === source || baseIsMatch(object, source, matchData);
   };
-}
-
-/**
- * Gets the value at `path` of `object`. If the resolved value is
- * `undefined`, the `defaultValue` is returned in its place.
- *
- * @static
- * @memberOf _
- * @since 3.7.0
- * @category Object
- * @param {Object} object The object to query.
- * @param {Array|string} path The path of the property to get.
- * @param {*} [defaultValue] The value returned for `undefined` resolved values.
- * @returns {*} Returns the resolved value.
- * @example
- *
- * var object = { 'a': [{ 'b': { 'c': 3 } }] };
- *
- * _.get(object, 'a[0].b.c');
- * // => 3
- *
- * _.get(object, ['a', '0', 'b', 'c']);
- * // => 3
- *
- * _.get(object, 'a.b.c', 'default');
- * // => 'default'
- */
-function get(object, path, defaultValue) {
-  var result = object == null ? undefined : baseGet(object, path);
-  return result === undefined ? defaultValue : result;
 }
 
 /**
@@ -4443,7 +4443,7 @@ var adapt = /*#__PURE__*/Object.freeze({
 });
 
 const evalmap = {
-  '!new function(){eval("this.a=1")}().a': 'false',
+  '!new function(){eval("this.a=1")}().a': () => false,
 };
 
 function evalHandle(config) {
@@ -4455,7 +4455,7 @@ function evalHandle(config) {
     apply(target, thisArg, params) {
       if (log) win.console.log(`【EVAL APPLY】参数：${params.map(it => self.getTools('compressText')(JSON.stringify(it)))}`);
       const new_params = params.map((param) => {
-        if (typeof evalmap[param] === 'string') return evalmap[param];
+        if (evalmap[param]) return evalmap[param];
         if (param.includes('debugger')) {
           param = param.replace(/debugger/g, '');
         }
@@ -5158,37 +5158,72 @@ function indexedDBHandle(config) {
 }
 
 function requestHandle(config) {
+  const self = this;
   if (typeof config !== 'object') config = {};
   const win = this.memory.window;
-  const { log, cb } = config;
+  self._registerConfig('handle:request', config, () => {
+    config.log = (l => {
+      if (!l || typeof l === 'function') return l;
+      return (type, ...p) => {
+        if (Array.isArray(l) && !l.includes(p[0])) return;
+        self.memory.window.console.log(`【REQUEST HANDLE】${type}: `, p[0]);
+      }
+    })(config.log);
+  });
   win.XMLHttpRequest = new win.Proxy(win.XMLHttpRequest, {
     construct(target, args, newTarget) {
-      return new win.Proxy(win.Reflect.construct(target, args, newTarget), {
-        set(target, prop, value, receiver) {
-          if (typeof value === 'function') {
-            return win.Reflect.set(target, prop, new win.Proxy(value, {
-              apply(fn, thisArg, params) {
-                if (thisArg.readyState === 4) {
-                  if (log) win.console.log(`【REQUEST ${prop} APPLY】链接：${xhr.responseURL}，状态码：${xhr.status}，返回数据；${xhr.responseText}`);
-                  if (cb) cb({
-                    name: prop,
-                    url: thisArg.responseURL,
-                    status: thisArg.status,
-                    response: thisArg.responseText
-                  });
-                }
-                return win.Reflect.apply(fn, thisArg, params);
-              }
-            }), receiver);
-          }
-          return win.Reflect.set(target, prop, value, receiver);
-        }
-      });
+      const xhr = win.Reflect.construct(target, args, newTarget);
+      xhr.__xhr = { headers: {} };
+      return xhr;
     }
   });
+  win.XMLHttpRequest.prototype.open = function (method, url, async) {
+    Object.assign(this.__xhr, { method, url, async });
+    config.log?.('open', this.__xhr);
+    config.cb?.('open', this.__xhr);
+    return self.memory['XMLHttpRequest.prototype.open'].apply(this, arguments);
+  };
+  win.XMLHttpRequest.prototype.setRequestHeader = function (key, value) {
+    this.__xhr.headers[key] = value;
+    config.log?.('header', this.__xhr);
+    config.cb?.('header', this.__xhr);
+    return self.memory['XMLHttpRequest.prototype.setRequestHeader'].apply(this, arguments);
+  };
+  win.XMLHttpRequest.prototype.send = function (body) {
+    this.__xhr.body = body;
+    config.log?.('send', this.__xhr);
+    config.cb?.('send', this.__xhr);
+    const xhr = this;
+    function onLoad() {
+      try {
+        Object.assign(xhr.__xhr, {
+          status: xhr.status,
+          responseURL: xhr.responseURL,
+          responseType: xhr.responseType,
+          response: xhr.responseType && xhr.responseType !== 'text'
+            ? xhr.response
+            : xhr.responseText
+        });
+        config.log?.('load', xhr.__xhr);
+        config.cb?.('load', xhr.__xhr);
+      } catch (e) {}
+    }
+    function onError() {
+      config.log?.('error', xhr.__xhr);
+      config.cb?.('error', xhr.__xhr);
+    }
+    this.addEventListener('load', onLoad);
+    this.addEventListener('error', onError);
+    return self.memory['XMLHttpRequest.prototype.send'].apply(this, arguments);
+  };
 }
 
-const requestInit = ['XMLHttpRequest'];
+const requestInit = [
+  'XMLHttpRequest',
+  'XMLHttpRequest.prototype.open',
+  'XMLHttpRequest.prototype.setRequestHeader',
+  'XMLHttpRequest.prototype.send'
+];
 
 function windowHandle(config = {}) {
   if (!this.config.isNode) {
@@ -5279,6 +5314,7 @@ function windowHandle(config = {}) {
           if (config.windowGetterUndefinedKeys.includes(prop)) return undefined;
           if (prop === 'eval') {
             return self.tools.setFuncNative((code) => {
+              if (typeof code === 'function') return code();
               if (!code.includes('window')) return realm.ori.eval(code);
               return runInContext(code, vm.createContext(realm.context), setting);
             }, 'eval', 1)
@@ -5397,9 +5433,7 @@ class index {
         handles[key].call(this, memory);
       } else if (Array.isArray(handles[key])) {
         for (let name of handles[key]) {
-          if (win[name]) {
-            memory[name] = win[name];
-          }
+          memory[name] = get(win, name) || null;
         }
       }
     }
